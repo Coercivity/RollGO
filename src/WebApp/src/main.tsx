3f643379bc15253034d994edaba6bcd1c2d0c747
Yaroslav Plekhanov <jar5000@yandex.ru>
2023-12-01 21:32:58 +0300

    Сделал Lobby

--- src/WebApp/src/main.tsx
+++ src/WebApp/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import Navbar from './components/Navbar';
+import MainPage from './views/MainPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
-    <App />
+    <MainPage />
   </React.StrictMode>,
 );
