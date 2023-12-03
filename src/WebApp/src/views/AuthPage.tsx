b619767a7d8bd443c384bfcb7f20a180489da2c6
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2023-12-03 19:52:16 +0300

    test pipeline

--- src/WebApp/src/views/AuthPage.tsx
+++ src/WebApp/src/views/AuthPage.tsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { Route } from '../enums/Route';
 
 const Auth = () => {
-  const location = useLocation();
+  const location = useLocatison();
   const isLogin = location.pathname === Route.LOGIN;
 
   return <Box>{isLogin ? <LogIn /> : <Registration />}</Box>;
