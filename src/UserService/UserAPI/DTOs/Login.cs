b8d663e06ce49712fff85848cea2450767908dba
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-01-12 22:13:04 +0300

    add ability to change user data

--- src/UserService/UserAPI/DTOs/Login.cs
+++ src/UserService/UserAPI/DTOs/Login.cs
@@ -10,10 +10,6 @@ public record LoginRequestDto
     [EmailAddress(ErrorMessage = "Invalid email format")]
     public string? Email { get; set; }
 
-    [Required(ErrorMessage = "The Password field is required")]
-    [StringLength(100, MinimumLength = 8, ErrorMessage = "The Password must be at least 8 characters")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$", 
-        ErrorMessage = "The Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character")]
     public required string Password { get; set; }
 }
 
--- src/UserService/UserAPI/Services/UserService.cs
+++ src/UserService/UserAPI/Services/UserService.cs
@@ -37,23 +37,17 @@ public class UserService(IUserRepository userRepository, IMapper mapper) : IUser
 
     public async Task<UserDto> CreateUser(CreateUserRequestDto dto)
     {
-        var usernameExists = await UsernameExists(dto.Username);
-        if (usernameExists)
-        {
-            throw new UserConflictException(ErrorCode.UsernameExists);
-        }
-        var emailExists = await EmailExists(dto.Email);
-        if (emailExists)
-        {
-            throw new UserConflictException(ErrorCode.EmailExists);
-        }
+        await CredentialsExists(dto.Username, dto.Email);
+
         var passwordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
         var user = await _userRepository.CreateAsync(new User()
         {
             Username = dto.Username,
             Email = dto.Email,
             Password = passwordHash,
         });
+
         return _mapper.Map<UserDto>(user);
     }
 
@@ -65,24 +59,48 @@ public class UserService(IUserRepository userRepository, IMapper mapper) : IUser
     public async Task<UserDto> GetUser(Guid id)
     {
         var user = await _userRepository.GetByIdAsync(id) ?? throw new UserNotFoundException(ErrorCode.UserNotFound);
+
         return _mapper.Map<UserDto>(user);
     }
 
     public async Task<UserDto> UpdateUser(UserDto dto)
     {
-        var user = await _userRepository.UpdateAsync(_mapper.Map<User>(dto)) ?? throw new UserNotFoundException(ErrorCode.UserNotFound);
-        return _mapper.Map<UserDto>(user);
+        var user = await _userRepository.GetByIdAsync(dto.Id) ?? throw new UserNotFoundException(ErrorCode.UserNotFound);
+
+        if (dto.Password != null && !BCrypt.Net.BCrypt.Verify(dto.CurrentPassword, user.Password))
+        {
+            throw new UserConflictException(ErrorCode.WrongPassword);
+        }
+
+        if ((dto.Email != user.Email || dto.Username != user.Username) && (dto.Username != null || dto.Email != null))
+        {
+            await CredentialsExists(dto.Username, dto.Email);
+        }
+
+        dto.Password = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
+        var updatedUser = await _userRepository.UpdateAsync(_mapper.Map<User>(dto)) ?? throw new UserNotFoundException(ErrorCode.UserNotFound);
+
+        return _mapper.Map<UserDto>(updatedUser);
     }
 
-    private async Task<bool> EmailExists(string email)
+    private async Task CredentialsExists(string? username, string? email)
     {
-        var user = await _userRepository.GetByEmailAsync(email);
-        return user != null;
-    }
-
-    private async Task<bool> UsernameExists(string username)
-    {
-        var user = await _userRepository.GetByUsernameAsync(username);
-        return user != null;
+        if (username != null)
+        {
+            var user = await _userRepository.GetByUsernameAsync(username);
+            if (user != null)
+            {
+                throw new UserConflictException(ErrorCode.UsernameExists);
+            }
+        }
+        if (email != null)
+        {
+            var user = await _userRepository.GetByEmailAsync(email);
+            if (user != null)
+            {
+                throw new UserConflictException(ErrorCode.EmailExists);
+            }
+        }
     }
 }
